3473ec234f93a22df35175890326d03ee4b7e70e
Nick Piper <nick.piper@cgi.com>
2010-09-06 15:06:42 +0000

    Add a plugin which does `svnadmin verify -r $REV` against each new revision, and has a `trac-admin` and API command to run a full verify against all revisions. Keeps results in a database table, with a simple admin page to show latest results for each repository. More features needed, but it can do for now. It doesn't send alerts - but we also don't send alerts for disk space, or anything else, etc. yet. Closes #265
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@1169 484df14f-75d2-4b3f-8ba0-490e05069a01

---
+++ svnverifyplugin/db.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Logica
+# All rights reserved.
+#
+
+from trac.core import Component, implements, TracError
+from trac.db import Table, Column, Index
+from trac.db import DatabaseManager
+from trac.env import IEnvironmentSetupParticipant
+from trac.util.datefmt import from_utimestamp, to_utimestamp, utc, utcmax, format_datetime
+
+from trac.util.translation import _
+
+import os
+import datetime
+
+schema_version = 1
+
+class SVNVerifyDB(Component):
+    implements(IEnvironmentSetupParticipant)
+    
+    # IEnvironmentSetupParticipant
+    SCHEMA = [
+        Table('svnverify_log', key=('id'))[
+            Column('id', auto_increment=True),
+            Column('repository_id', type='int'),
+            Column('type'),            
+            Column('result', type='int'),            
+            Column('log'),
+            Column('time', type='int64'),
+            Index(['repository_id']),
+            Index(['type']),
+            Index(['result']),            
+            Index(['time'])],
+        ]
+
+    def environment_created(self):
+        self._upgrade_db(self.env.get_db_cnx())
+
+    def environment_needs_upgrade(self, db):
+        cursor = db.cursor()
+
+        cursor.execute("SELECT value FROM system WHERE name='svnverify_version'")
+        row = cursor.fetchone()
+        if row:
+            return False
+        return True
+
+    def upgrade_environment(self, db):
+        self._upgrade_db(db)
+
+    def _upgrade_db(self, db):
+        try:
+            db_backend, _ = DatabaseManager(self.env)._get_connector()
+            cursor = db.cursor()
+            for table in self.SCHEMA:
+                for stmt in db_backend.to_sql(table):
+                    self.log.debug(stmt)
+                    cursor.execute(stmt)
+                    db.commit()
+            cursor.execute("INSERT INTO system (name, value) VALUES ('svnverify_version',%s)", str(schema_version))
+        except Exception, e:
+            db.rollback()
+            self.log.error(e, exc_info=True)
+            raise TracError(str(e))
+
